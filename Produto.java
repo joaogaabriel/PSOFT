07619cddf7207b07a33f9cba1821541a53fbaf28
joao.ferreira <joaogabriel13ferreira@gmail.com>
2022-10-03 09:38:24 -0300

    segundo commit

--- Produto.java
+++ Produto.java
@@ -1,3 +1,12 @@
 public class Produto {
+    private String nome;
+    private String fabricante;
+    private double valor;
 
+    public Produto(String nome, String fabricante, double valor){
+        this.nome = nome;
+        this.fabricante = fabricante;
+        this.valor = valor;
+
+    }
 }
